b25e7288a77ede7e0a1cef286e070852953a119c
Deivison Jimenez <deivi.jimenez@gmail.com>
2023-11-21 12:00:39 -0400

    Creacion de practica de .net y RabbitMQ con Docker

---
+++ NetRabbitMQ/Consumer/Receiver.cs
@@ -0,0 +1,31 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "vaxidrez",
+    Password = "VaxiDrez2005",
+
+};
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare("VaxiQueue", false, false, false, null);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.Span;
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine("Mensaje Recibido {0}", message);
+    };
+
+    channel.BasicConsume("VaxiQueue", true, consumer);
+
+    Console.WriteLine("Presiona [Enter] para salir del consumer");
+    Console.ReadLine();
+}
---
+++ NetRabbitMQ/Producer/Sender.cs
@@ -0,0 +1,28 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    HostName = "localhost",
+    UserName = "vaxidrez",
+    Password = "VaxiDrez2005",
+
+};
+
+using (var connection = factory.CreateConnection()) 
+using (var channel = connection.CreateModel()) 
+{
+    channel.QueueDeclare("VaxiQueue", false, false, false, null);
+
+    var message = "Bienvenido al curso RabbitMQ y .Net";
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish("", "VaxiQueue", null, body);
+
+    Console.WriteLine("El mensaje fue enviado{0}", message);
+}
+
+Console.WriteLine("Presiona [enter] para salir de la aplicacion");
+Console.ReadLine();
